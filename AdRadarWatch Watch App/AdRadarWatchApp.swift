c601b27e10cb17a2b05b5e65ccb2c3db2cca3d68
gdelfava <62187627+gdelfava@users.noreply.github.com>
2025-06-23 15:11:06 +0200

    1.9.5.5 Added Apple Watch App

---
+++ AdRadarWatch Watch App/AdRadarWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdRadarWatchApp.swift
+//  AdRadarWatch Watch App
+//
+//  Created by Guilio Del Fava on 2025/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AdRadarWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
